ae8af41c1562257e474bee6ee57e145f9c5c3667
Danieljohansen97 <danieljohansen1997@hotmail.com>
2024-11-24 14:23:14 +0100

    Add links to Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes } from "react-router-dom";
 
-import './App.module.scss'
-import CustomNavbar from './components/navbar/CustomNavbar'
-import HomePage from './pages/homepage/HomePage'
+import "./App.module.scss";
+import CustomNavbar from "./components/navbar/CustomNavbar";
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import Training from "./pages/training/Training";
 
 function App() {
   return (
     <>
       <CustomNavbar />
       <div>
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-        </Routes>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/training" element={<Training />} />
+          </Routes>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/navbar/CustomNavbar.tsx
+++ src/components/navbar/CustomNavbar.tsx
@@ -1,11 +1,71 @@
 import { Link } from "react-router-dom";
-
-import "./CustomNavbar.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCalendarDays,
+  faCircleInfo,
+  faDumbbell,
+  faHandFist,
+  faHouse,
+  faRightFromBracket,
+  faRightToBracket,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
+import styles from "./CustomNavbar.module.scss";
 
 export default function CustomNavbar() {
   return (
-    <div>
-      <Link to="/">Hjem</Link>
+    <div className={styles["nav-main"]}>
+      <div className={styles["nav-section"]}>
+        <div className={styles["link-container"]}>
+          <Link to="/">
+            <FontAwesomeIcon icon={faHouse} />
+            <span>Hjem</span>
+          </Link>
+        </div>
+        <div className={styles["link-container"]}>
+          <Link to="/about">
+            <FontAwesomeIcon icon={faCircleInfo} />
+            <span>Om oss</span>
+          </Link>
+        </div>
+        <div className={styles["link-container"]}>
+          <Link to="/training">
+            <FontAwesomeIcon icon={faDumbbell} />
+            <span>Trening</span>
+          </Link>
+        </div>
+        <div className={styles["link-container"]}>
+          <Link to="/program">
+            <FontAwesomeIcon icon={faCalendarDays} />
+            <span>Program</span>
+          </Link>
+        </div>
+      </div>
+
+      <div className={styles["nav-section"]}>
+        <h1 className={styles["nav-title"]}>Oslo Fullkontakt Karate</h1>
+      </div>
+
+      <div className={styles["nav-section"]}>
+        <div className={styles["link-container"]}>
+          <Link to="/user">
+            <FontAwesomeIcon icon={faUser} />
+            <span>Min side</span>
+          </Link>
+        </div>
+        <div className={styles["link-container"]}>
+          <Link to="/login">
+            <FontAwesomeIcon icon={faRightToBracket} />
+            <span>Logg inn</span>
+          </Link>
+        </div>
+        <div className={styles["link-container"]}>
+          <Link to="/logout">
+            <FontAwesomeIcon icon={faRightFromBracket} />
+            <span>Logg ut</span>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
