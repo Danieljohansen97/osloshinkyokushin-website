3bd98024167749463b5cfb6ac84097c7f6956f54
Odd Karsten Svartaas <oksvart@gmail.com>
2025-05-20 13:26:37 +0200

    Set up aliases for components and utilities
    Install packages needed for tests
    Consolidate tsconfig into "regular" and "node" (this was a result of Vite being a bit picky when it comes to setting up aliases)

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '#': path.resolve(__dirname, './src'),
+      '#assets': path.resolve(__dirname, './src/assets'),
+      '#lib': path.resolve(__dirname, './src/lib'),
+      '#helpers': path.resolve(__dirname, './src/lib/helpers'),
+      '#contexts': path.resolve(__dirname, './src/contexts'),
+      '#components': path.resolve(__dirname, './src/components'),
+      '#pages': path.resolve(__dirname, './src/pages'),
+      '#testUtils': path.resolve(__dirname, './src/tests/testUtils.tsx'),
+      '#libTestUtils': path.resolve(__dirname, './src/lib/tests/testUtils.ts'),
+      '#hooks': path.resolve(__dirname, './src/hooks'),
+    },
+  },
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    passWithNoTests: true,
+    setupFiles: ['./src/tests/setup.ts'],
+  },
   css: {
     preprocessorOptions: {
       scss: {
-        api: 'modern-compiler'
-      }
-    }
+        api: 'modern-compiler',
+      },
+    },
   },
-})
+});
